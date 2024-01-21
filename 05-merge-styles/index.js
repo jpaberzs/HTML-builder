cba9dc1ce55db08e32558428768ab0e507244e66
jpaberzs <janis.paberzs18@gmail.com>
2024-01-21 21:20:45 +0200

    feat: done 05 task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+let result = '';
+
+function readdir(file) {
+  if (path.parse(file).ext == '.css') {
+    const readerStream = fs.createReadStream(
+      path.join(__dirname, `/styles/${file}`),
+      'utf8',
+    );
+    readerStream.on('data', (chunk) => {
+      result += chunk;
+    });
+    readerStream.on('end', () => {
+      fs.appendFile(
+        path.join(__dirname, 'project-dist/bundle.css'),
+        result,
+        (err) => {
+          if (err) throw err;
+        },
+      );
+    });
+    readerStream.on('error', (err) => {
+      console.log(err.stack);
+    });
+  }
+}
+
+// fs.mkdir(path.join(__dirname, 'styles'), { recursive: true }, (err) => {
+//   if (err) return console.log(err);
+// });
+fs.readdir(path.join(__dirname, '/styles/'), (err, files) => {
+  if (err) throw err;
+  // console.log(files);
+  fs.readFile(path.join(__dirname, 'project-dist/bundle.css'), (err) => {
+    if (!err) {
+      fs.writeFile(
+        path.join(__dirname, 'project-dist/bundle.css'),
+        '',
+        () => {},
+      );
+    }
+  });
+  files.forEach((file) => {
+    readdir(file);
+  });
+});
